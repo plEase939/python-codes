d49b07a5493e53081e2517471a21b781f6ec891c
Shailesh Pathak <48568048+plEase939@users.noreply.github.com>
2022-07-24 18:43:19 +0530

    Rock paper scissor Game on python

---
+++ rock_paper_scissor.py
@@ -0,0 +1,36 @@
+import random
+
+play =1
+playerScore = 0
+computerScore = 0
+
+possibleActions = ("rock", "paper", "scissor")
+
+while play == 1:
+    play = input("Play? y/n: ")
+    if play.upper() != "N":
+        play = 1
+    else:
+        print("Game End")
+        break
+    userAction = input("Choose your move rock or paper or scissor: ")
+    computerAction = random.choice(possibleActions)
+    if userAction == computerAction:
+        print("You chose " + userAction + " and computer chose " + computerAction + "\n")
+        print("It's a draw!")
+    elif userAction == "rock" and computerAction == "scissor":
+        print("You chose " + userAction + " computer chose " + computerAction + "\n You win!")
+        playerScore +=1
+        print("Score: " + " Your Score: " + str(playerScore) + " Computer Score: " + str(computerScore))
+    elif userAction == "paper" and computerAction == "rock":
+        print("You chose " + userAction + " computer chose " + computerAction + " \n You win!")
+        playerScore +=1
+        print("Score: " + " Your Score: " + str(playerScore) + " Computer Score: " + str(computerScore))
+    elif userAction == "scissor" and computerAction == "paper":
+        print("You chose " + userAction + " computer chose " + computerAction + "\n You win!")
+        playerScore +=1
+        print("Score: " + " Your Score: " + str(playerScore) + " Computer Score: " + str(computerScore))
+    else:
+        print("you chose " + userAction + " computer chose " + computerAction + " \n You Lose!")
+        computerScore += 1
+        print("Score: " + " Your Score: " + str(playerScore) + " Computer Score: " + str(computerScore))
