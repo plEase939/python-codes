694eca283173e93e2cd58dd4a79e604961a3c429
Shailesh Pathak <16tryhard16@gmail.com>
2022-07-24 21:26:23 +0530

    Optimized and better change.py
    Added new Change.py optimized code to add more functionalities

---
+++ Change.py
@@ -0,0 +1,63 @@
+
+
+
+currency = []
+
+def appendFunction():
+    totalDenom = int(input("Enter total number of Denominations: "))
+    count = 1
+    while totalDenom >= count:
+        value = int(input("Enter Denomination " + str(count) + " : "))
+        currency.append(value)
+        count += 1
+    showCurrency = input(print("display denominations Value? y/n : "))
+    if showCurrency.upper() == "Y":
+        print(currency)
+
+
+def removeFunction():
+    showCurrency = input(print("display denominations Value? y/n : "))
+    if showCurrency.upper() == "Y":
+        print(currency)
+    else:
+        removeWhat = int(input("Remove what denomination?: "))
+        index = currency.index(removeWhat)
+        currency.remove(index)
+        print(str(removeWhat) + "Was Removed from the List")
+
+def changeFunction(targetValue):
+    count = len(currency)
+    loopCount = 1
+    index_zero = 0 - count
+    index = 0
+    while index >= -6:
+        index = -1
+
+        
+
+
+print("Loading...")
+
+
+while True:
+    print("Welcome to Change allocator")
+    reset = input(print("Do you want to Reset or Add or Remove a Denomination(s) or Generate change? reset/add/remove/change/exit: "))
+    if reset.upper() == "RESET":
+        currency.clear()
+        add = input(print("Database Reseted! Do you want to add new Denominations? y/n :"))
+        if add.upper() == "Y":
+            appendFunction()
+    elif reset.upper() == "ADD":
+        appendFunction()
+    elif reset.upper() == "REMOVE":
+        removeFunction()
+    elif reset.upper() == "CHANGE":
+        targetValue = int(input(print("Enter Target Value: ")))
+        changeFunction(targetValue)
+    else:
+        exit = input(print("Input error - Try Again or type exit or press enter"))
+        if exit.upper() == "EXIT":
+            break
+        else:
+            continue
+print("Thank you for using Pathak's Change Generator")
--- Currency_and_target.py
+++ Currency_and_target.py
@@ -5,7 +5,15 @@
 
 currency =[]
 
+
+
 def currencyAdd(totalDenom):
+    reset = input("Do you want to reset the database or add or remove a denomination(s)")
+    if reset.upper() == "RESET":
+        currency.clear()
+    elif reset.upper() == "ADD":
+        
+
     count = 1
     while totalDenom >= count:
         value = int(input("Enter Denomination " + str(count) + " : "))
@@ -16,22 +24,22 @@ def Target(targetValue):
     if targetValue == 0:
         print("No changed as value is 0")
     
+while True:
+    totalDenom = int(input("Enter total number of Denominations: "))
+    call = input("Add/Update/remove new denominations to the memory? y/n :  ")
+    if call.upper() == "Y":
+        currencyAdd(totalDenom)
+    else:
+        print("Rejected by user")
+
+    print(currency)
+
+    quest = input("Do you want to put a target Value to generate Change? y/n:  ")
+    if quest.upper() == "Y":
+        targetValue = int(input("Enter Target Value: "))
+        Target(targetValue)
+    else:
+        print("Rejected by user")
 
 
 
-totalDenom = int(input("Enter total number of Denominations: "))
-
-call = input("Call append function? y/n :  ")
-if call.upper() == "Y":
-    currencyAdd(totalDenom)
-else:
-    print("Rejected by user")
-
-print(currency)
-
-quest = input("Do you want to put a target Value to generate Change? y/n:  ")
-if quest.upper() == "Y":
-    targetValue = int(input("Enter Target Value: "))
-    Target(targetValue)
-else:
-    print("Rejected by user")
